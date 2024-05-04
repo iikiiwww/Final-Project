2da022b75abe54882dd6c1da88461def01d6cb0f
Mr.Anggara <162547743+MuhamadRonaAnggara@users.noreply.github.com>
2024-05-04 21:44:49 +0700

    Final Project

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const navbarNav = document.querySelector
 document.querySelector('#iqoo-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 };
-
 // Click on outside sidebar for hide the menu
 const iqoo = document.querySelector
     ('#iqoo-menu');
